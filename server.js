8eb2921638fb7e4871353fa92e453789dfd616a3
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-06-16 15:56:07 -0700

    initial file structure and install

---
+++ server.js
@@ -0,0 +1,5 @@
+const connection = require('./config/connection');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const chalk = require('chalk');
+const validate = require('./javascript/validate');
